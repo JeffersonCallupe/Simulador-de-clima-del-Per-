f2a95691e5ca4757548f9b2bcd65ce066fd0775e
Jefferson J. <130589639+JeffersonCallupe@users.noreply.github.com>
2024-08-17 10:36:17 -0500

    v2. primera entrega
    Dentro de esta segunda versión de la primera entrega se hace las siguientes modificaciones:
     - Se añadió el uso de la función flecha. 
     - Se añadió el condicional ternario.

--- Assets/index.js
+++ Assets/index.js
@@ -2,36 +2,43 @@
 // Lista de ciudades 
 let ciudades = ["Lima", "Cusco", "Arequipa", "Trujillo", "Iquitos", "Cajamarca", "Ayacucho"];
 
+/*
+Haciendo una búsqueda de ciudad mediante el ciclo for y codicionales
+dentro de una función. También se ha incluido la función flecha
+y el uso de condición ternario.
+*/
 
-// Haciendo una búsqueda de ciudad mediante el ciclo for y codicionales
 
-let buscarCiudad = "Lima";  // Búsqueda por variable. 
-let ciudadNoEncontrada = false;
-
-
-for(let i=0 ; i < ciudades.length; i++){
-    if ( buscarCiudad === ciudades[i]){
-        console.log (`${buscarCiudad} está dentro de la lista de ciudades`);
-    }else{
-        ciudadNoEncontrada = true;
+const buscarCiudad = ciudad => {
+    let encontrada = false;
+    for (let i = 0; i < ciudades.length; i++) {
+        if (ciudad === ciudades[i]) {
+            encontrada = true;
+            break; 
+        }
     }
-}
+    return alert(`${ciudad} ${encontrada===true ? 'está' : 'no está'} dentro de la lista de ciudades`);
+};
 
-if(ciudadNoEncontrada === true){
-    console.log (`${buscarCiudad} no está dentro de la lista de ciudades`);
-}
+let ciudad = prompt("Indique una ciudad");
+buscarCiudad(ciudad);
 
 
 
 // SEGUNDA FORMA A CONSIDERAR
 
 /*
-if ( ciudades.includes(buscarCiudad)){
-    console.log (`${buscarCiudad} está dentro de la lista de ciudades`);
-}else{
-    console.log (`${buscarCiudad} no está dentro de la lista de ciudades`);
-}
+const buscarCiudad = ciudad => 
+    ciudades.includes(ciudad) ? 
+        alert(`${ciudad} está dentro de la lista de ciudades`) :
+        alert(`${ciudad} no está dentro de la lista de ciudades`);
+
+let ciudad = prompt("Indique una ciudad");
+buscarCiudad(ciudad);
 */
+
+
+
 
 
 
